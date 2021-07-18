29f5e3b58b05c198fb722781dde99d621e165dd1
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-07-18 16:36:24 +0900

    [add]スタミナ増減処理を追加

--- src/main/java/com/github/kamunyan/zombiefloor/listeners/PlayerControlListener.java
+++ src/main/java/com/github/kamunyan/zombiefloor/listeners/PlayerControlListener.java
@@ -5,6 +5,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
 
 
 /**
@@ -31,5 +33,26 @@ public class PlayerControlListener implements Listener {
         if (!e.getPlayer().isOp()) {
             e.setCancelled(true);
         }
+    }
+
+    /**
+     * スプリントと歩行の切り替えによって、プレイヤーのスタミナを増減する
+     */
+    @EventHandler
+    public void onToggleSplint(PlayerToggleSprintEvent e) {
+        var player = e.getPlayer();
+        if (e.isSprinting()) {
+            plugin.getManager().getZFPlayer(player).runStaminaDecreaseRunnable();
+        } else {
+            plugin.getManager().getZFPlayer(player).runStaminaRecoveryRunnable();
+        }
+    }
+
+    /**
+     * 通常の空腹ゲージ増減を制限する
+     */
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
     }
 }
