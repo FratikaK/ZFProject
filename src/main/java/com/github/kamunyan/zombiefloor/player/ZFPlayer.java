14a4fbb1aeb5e99104e06605b05d73a549d0963b
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-07-18 16:37:38 +0900

    [add]スタミナ関係の処理を追加

--- src/main/java/com/github/kamunyan/zombiefloor/player/ZFPlayer.java
+++ src/main/java/com/github/kamunyan/zombiefloor/player/ZFPlayer.java
@@ -1,22 +1,47 @@
 package com.github.kamunyan.zombiefloor.player;
 
 import com.github.kamunyan.zombiefloor.ZombieFloor;
+import com.github.kamunyan.zombiefloor.player.runnable.StaminaDecreaseRunnable;
+import com.github.kamunyan.zombiefloor.player.runnable.StaminaRecoveryRunnable;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
+/**
+ * ZombieFloorにおけるプレイヤーを表現するクラス
+ */
 public class ZFPlayer {
     private final ZombieFloor plugin = ZombieFloor.getInstance();
     //プレイヤーのPlayer型
-    private Player player;
+    private final Player player;
     //プレイヤーのUUID
-    private UUID uuid;
+    private final UUID uuid;
     //マッチプレイヤーか
     private Boolean matchPlayer = false;
+    //スタミナ減少速度
+    private int staminaDecreaseSpeed = 8;
+    //スタミナ回復速度
+    private int staminaRecoverySpeed = 12;
+    //スタミナが回復し始めるまでの遅延時間
+    private int delayStaminaRestoreTime = 60;
 
-    public ZFPlayer(UUID uuid){
+    public ZFPlayer(UUID uuid) {
         player = plugin.getServer().getPlayer(uuid);
-        uuid = this.uuid;
+        this.uuid = uuid;
+    }
+
+    public void runStaminaDecreaseRunnable() {
+        new StaminaDecreaseRunnable(player).runTaskTimerAsynchronously(
+                plugin,
+                0,
+                staminaDecreaseSpeed);
+    }
+
+    public void runStaminaRecoveryRunnable() {
+        new StaminaRecoveryRunnable(player).runTaskTimerAsynchronously(
+                plugin,
+                delayStaminaRestoreTime,
+                staminaRecoverySpeed);
     }
 
     @Override
@@ -32,6 +57,10 @@ public class ZFPlayer {
         return player;
     }
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
     public Boolean isMatchPlayer() {
         return matchPlayer;
     }
@@ -40,4 +69,15 @@ public class ZFPlayer {
         this.matchPlayer = matchPlayer;
     }
 
+    public void setStaminaDecreaseSpeed(int staminaDecreaseSpeed) {
+        this.staminaDecreaseSpeed = staminaDecreaseSpeed;
+    }
+
+    public void setStaminaRecoverySpeed(int staminaRecoverySpeed) {
+        this.staminaRecoverySpeed = staminaRecoverySpeed;
+    }
+
+    public void setDelayStaminaRestoreTime(int delayStaminaRestoreTime) {
+        this.delayStaminaRestoreTime = delayStaminaRestoreTime;
+    }
 }
