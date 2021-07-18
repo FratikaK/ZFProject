b917d9690ae7cf507cbdead09d687851cc0f35d0
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-07-18 16:36:01 +0900

    [add]ZFPlayerを取得する処理を追加

--- src/main/java/com/github/kamunyan/zombiefloor/MatchManager.java
+++ src/main/java/com/github/kamunyan/zombiefloor/MatchManager.java
@@ -1,6 +1,7 @@
 package com.github.kamunyan.zombiefloor;
 
 import com.github.kamunyan.zombiefloor.player.ZFPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -25,10 +26,26 @@ public class MatchManager {
      * @param player 格納したいプレイヤー
      */
     public synchronized void addGamePlayer(Player player) {
-        if (gamePlayers.containsKey(player.getUniqueId())) {return;}
-        gamePlayers.put(player.getUniqueId(),new ZFPlayer(player.getUniqueId()));
-        plugin.sendLog(player.getDisplayName()+"のZFPlayerを作成しました");
-        plugin.sendLog(gamePlayers.get(player.getUniqueId()).toString());
+        if (gamePlayers.containsKey(player.getUniqueId())) {
+            return;
+        }
+        gamePlayers.put(player.getUniqueId(), new ZFPlayer(player.getUniqueId()));
+        Bukkit.getLogger().info(player.getDisplayName() + "のZFPlayerを作成しました");
+        Bukkit.getLogger().info(gamePlayers.get(player.getUniqueId()).toString());
+    }
+
+    /**
+     * ZFPlayerを取得する
+     * なければZFPlayerを作成してreturn
+     *
+     * @param target 対象のプレイヤー
+     * @return ZFPlayer
+     */
+    public ZFPlayer getZFPlayer(Player target) {
+        if (!gamePlayers.containsKey(target.getUniqueId())) {
+            addGamePlayer(target);
+        }
+        return gamePlayers.get(target.getUniqueId());
     }
 
     public ArrayList<ZFPlayer> getMatchPlayers() {
