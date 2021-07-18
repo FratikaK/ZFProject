0cdd4ab753ef51a4a1bc48a271dcd2ab343c0af1
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-07-18 16:38:00 +0900

    [null]バグ修正

--- src/main/java/com/github/kamunyan/zombiefloor/ZombieFloor.java
+++ src/main/java/com/github/kamunyan/zombiefloor/ZombieFloor.java
@@ -2,11 +2,8 @@ package com.github.kamunyan.zombiefloor;
 
 import com.github.kamunyan.zombiefloor.listeners.JoinQuitListener;
 import com.github.kamunyan.zombiefloor.listeners.PlayerControlListener;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 public final class ZombieFloor extends JavaPlugin {
 
@@ -23,24 +20,6 @@ public final class ZombieFloor extends JavaPlugin {
 
     @Override
     public void onDisable() {
-    }
-
-    /**
-     * 通常のlogを出力する
-     *
-     * @param log 出力したいログ
-     */
-    public void sendLog(@NotNull String log) {
-        Bukkit.getLogger().info(ChatColor.AQUA + log);
-    }
-
-    /**
-     * 警告を意味するlogを出力する
-     *
-     * @param warnLog 出力したい警告のlog
-     */
-    public void sendWarnLog(@NotNull String warnLog) {
-        Bukkit.getLogger().info(ChatColor.RED + warnLog);
     }
 
     /**
